cb66bfdbebbc98c12c5694875c647db36346fc2b
Killjoy <aryaajitnair@gmail.com>
2022-11-29 01:51:51 +0530

    feat:Use pickle model to get prediction

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask,request, render_template
+import pickle
+import numpy as np
 
-
+model = pickle.load(open('model.pkl','rb'))
 
 app=Flask(__name__)
 
@@ -8,7 +10,9 @@ app=Flask(__name__)
 @app.route('/submit',methods=['POST'])
 def userInput():
     data = request.form.to_dict(flat=False)
-    return data
+    prediction=model.predict([[1,75,75,80,1,1,0,0,1,0,1,1,1,1,0,1]])
+    print(prediction)
+    return {"predicted_val":int(prediction[0])}
 
 if __name__=="__main__":
     app.run(debug=True)
